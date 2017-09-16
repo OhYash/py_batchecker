61551f3d0691062816d380c7b0b961f87626b91e
Yash Yadav <yashdimpu@gmail.com>
2017-09-16 23:31:13 +0530

    Notification description now displays battery %

--- batchecker.py
+++ batchecker.py
@@ -7,7 +7,10 @@ while(True) :
 
     output = subprocess.check_output(['bash','-c', "acpi"])
     outlist = output.split()
+    subsentence = "Battery : "+outlist[3].decode("utf-8")
+    subsentence = subsentence[:-2]+'%'
+    print(subsentence)
 
     if outlist[2] == b'Discharging,' :
-        subprocess.call(['notify-send', 'Charger Removed', 'Please reinsert it']);
-    time.sleep(900)
+        subprocess.call(['notify-send', 'Charger Removed', subsentence])
+    time.sleep(600)
