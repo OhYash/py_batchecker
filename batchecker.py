fafc9b235eefaf354e69f0ce80dae50e3fae5d99
Yash Yadav <yashdimpu@gmail.com>
2017-09-26 23:37:05 +0530

    Added reminder that asks user to remove the charger when battery > 90%

--- batchecker.py
+++ batchecker.py
@@ -7,10 +7,11 @@ while(True) :
 
     output = subprocess.check_output(['bash','-c', "acpi"])
     outlist = output.split()
-    subsentence = "Battery : "+outlist[3].decode("utf-8")
-    subsentence = subsentence[:-2]+'%'
-    print(subsentence)
+    subsentence = "Battery : " \
+    + outlist[3].decode("utf-8")
 
     if outlist[2] == b'Discharging,' :
         subprocess.call(['notify-send', 'Charger Removed', subsentence])
+    elif int(outlist[3].decode("utf-8")[:-2]) >= 90  :
+        subprocess.call(['notify-send', 'Remove Charger', subsentence])
     time.sleep(600)
